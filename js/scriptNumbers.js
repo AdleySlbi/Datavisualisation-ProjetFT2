4e639ec6ce9a5b4dd2557b2eb404c18269b600a1
Adley <adley.salabi@gmail.com>
2018-01-21 22:03:03 +0100

    Ajout du premier Chart

---
+++ js/scriptNumbers.js
@@ -0,0 +1,35 @@
+{
+    "gameMode" : {
+        "online": {
+            "inviteReceived": 11640,
+            "inviteSent": 11604,
+            "matchmaking": 1356,
+            "total": 24600,
+            "victoire": 8972,
+            "defaite": 7502,
+            "quit": 4711,
+            "connexion": 2568
+        },
+        "story": {
+            "easy": 72951,
+            "medium": 280323,
+            "hard": 53312,
+            "total": 405586
+        },
+        "solo": {
+            "easy": 66676,
+            "medium": 35256,
+            "hard": 56524,
+            "expert": 30153,
+            "total": 188609
+        },
+        "passAndPlay": {
+            "totalEnd": 1825,
+            "totalNotEnd": 6267,
+            "moyHandGame": 20.7 ,
+            "total": 14030
+        },
+    }
+}
+
+
